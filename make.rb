bebb80b2ab7a0d0227dc9a1b1d59ece3e3c061a7
Paul Harrington <xeno@Clover.(none)>
2010-07-23 00:36:51 -0400

    initial viewport

--- make.rb
+++ make.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 
 # just concatenate our files
-files = %w[intro main sprite quadtree world outro].map { |fn| "#{fn}.js" }
+files = %w[intro main sprite quadtree world viewport outro].map { |fn| "#{fn}.js" }
 bodies = "bodies.js"
 File.open(bodies, "w") do |f|
   files.each { |fn| f.write(File.read(fn)) }
