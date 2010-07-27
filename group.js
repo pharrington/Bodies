c9595249a8cf3de9b394e9e367f04c1e44d5095d
Paul Harrington <xeno@Clover.(none)>
2010-07-26 21:37:10 -0400

    add Group class

---
+++ group.js
@@ -0,0 +1,21 @@
+$.Group = function () {
+};
+
+$.Group.prototype.items = [];
+
+$.Group.prototype.insert = function (item) {
+	this.items[this.items.length] = item;
+};
+
+$.Group.prototype.remove = function (item) {
+	var index, items = this.items;
+	if ((index = items.indexOf(item)) === -1) { return; }
+	items.splice(index, 1);
+};
+
+$.Group.draw = function () {
+	var items = this.items;
+	for (var i = items.length - 1; i; --i) {
+		items[i].draw();
+	}
+};
