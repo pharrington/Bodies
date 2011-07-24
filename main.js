41cddad450b74097f6b167707e95a2cb76545009
Paul Harrington <pharrington@badenoughdu.de>
2011-07-24 03:42:07 -0400

    call preventDefault on key handlers

--- main.js
+++ main.js
@@ -420,10 +420,12 @@ function attachEvents() {
 			keyPress && keyPress(e.keyCode);
 		}
 
+		e.preventDefault();
 	}, false);
 	
 	addEventListener("keyup", function (e) {
 		$.keys[e.keyCode] = false;
+		e.preventDefault();
 	}, false);
 }
 
