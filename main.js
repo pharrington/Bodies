ce85d159f51fd369d64baa3b350b9d7e517d6c6f
Paul Harrington <pharrington@badenoughdu.de>
2011-09-11 10:36:55 -0400

    conditional preventDefault for keyPress

--- main.js
+++ main.js
@@ -426,10 +426,12 @@ function attachEvents() {
 	
 		if (!pressed) {
 			keys[e.keyCode] = {at: now, last: now + keyInterval};
-			keyPress && keyPress(e.keyCode);
+			if (keyPress) {
+				if (keyPress(e.keyCode) === false) {
+					e.preventDefault()
+				}
+			};
 		}
-
-		e.preventDefault();
 	}, false);
 	
 	addEventListener("keyup", function (e) {
