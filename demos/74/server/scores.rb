c9540510d4a7d9000845ac5ba85f07631642ee22
unknown <Paul@.(none)>
2012-09-05 15:51:47 -0400

    verify the page and per_page query values when fetching scores; add support for Time Attack scores

--- demos/74/server/scores.rb
+++ demos/74/server/scores.rb
@@ -22,13 +22,20 @@ end
 
 get "/scores" do
   scores = nil
-  mode = "Normal"
-  page = (params[:page] || 1).to_i - 1
-  per_page = params[:per_page].to_i || 10
-  per_page = 10 if per_page > 100
-  index = page * per_page
+  mode, reverse_sort = mode_tuple params["mode"]
+  scores_key = "sorted_scores:#{mode}"
 
-  ids = redis.zrevrange "sorted_scores:#{mode}", index, index + per_page
+  per_page = clamp params[:per_page].to_i, 1, 10
+  total_pages = (redis.zcard(scores_key).to_f / per_page).ceil
+  total_pages = 1 if total_pages < 1
+  page = clamp params[:page].to_i, 1, total_pages
+  index = (page - 1) * per_page
+
+  if reverse_sort
+    ids = redis.zrevrange scores_key, index, index + per_page - 1
+  else
+    ids = redis.zrange scores_key, index, index + per_page - 1
+  end
 
   redis.pipelined do
     scores = ids.collect do |id|
@@ -36,19 +43,30 @@ get "/scores" do
     end
   end
 
-  scores.collect do |s|
+  scores.collect! do |s|
     score = s.last.value
     score.delete "replay_filepath"
     score[:key] = s.first
     score
-  end.to_json
+  end
+
+  {
+    page: page,
+    per_page: per_page,
+    pages: total_pages,
+    scores: scores
+  }.to_json
 end
 
 get "/dailybest" do
   score = nil
-  mode = "Normal"
+  mode, reverse_sort = mode_tuple params["mode"]
 
-  id = redis.zrevrange("daily_sorted_scores:#{mode}:#{today_str}", 0, 0)
+  if reverse_sort
+    id = redis.zrevrange("daily_sorted_scores:#{mode}:#{today_str}", 0, 0)
+  else
+    id = redis.zrange("daily_sorted_scores:#{mode}:#{today_str}", 0, 0)
+  end
 
   if id.empty?
     return score.to_json
@@ -75,11 +93,11 @@ post "/score" do
 
   redis.hmset "score:#{id}",
     "score", score,
-     "elapsed", params[:elapsed],
-     "player", params[:player],
-     "mode", mode,
-     "replay_filepath", save_replay(params[:replay], hash),
-     "date", date
+    "elapsed", params[:elapsed],
+    "player", params[:player],
+    "mode", mode,
+    "replay_filepath", save_replay(params[:replay], hash),
+    "date", date
 
   redis.zadd "daily_sorted_scores:#{mode}:#{today_str}", score, id
   redis.zadd "sorted_scores:#{mode}", score, id
@@ -108,4 +126,23 @@ end
 def redis
   @redis = Redis.new unless @redis
   @redis
+end
+
+def clamp value, min, max
+  if value < min
+    return min
+  elsif value > max
+    return max
+  else
+    return value
+  end
+end
+
+def mode_tuple str
+  modes = ["Normal", "TimeAttack"]
+  mode = str
+
+  mode = "Normal" unless modes.include? mode
+  reverse_sort = true unless mode == "TimeAttack"
+  [mode, reverse_sort]
 end
